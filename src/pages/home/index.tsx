966ff6038a042c0dbf7242aa84e2b4f02962e44f
joa7b <joa7@protonmail.com>
2023-01-17 15:30:41 -0300

    feat(home): create structure and install styledcom

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,17 @@
+
+// export class Home {
+//     render() {
+//         return(
+//             <h1>hello home</h1>
+//         )
+//     }
+// }
+
+
+const Home = () => {
+    return(
+        <h1>hello home</h1>
+    )
+}
+
+export default Home;
