aac7ff1abc21642006fe55d1c87c7ab4de10a0a1
joa7b <joa7@protonmail.com>
2023-01-24 20:54:35 -0300

    fix: chenge name of directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
-import Dashboard from "./pages/dashboard";
+import Dashboard from "./pages/login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/home" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Dashboard />} />
       </Routes>
     </Router>
   );
